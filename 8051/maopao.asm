4422203ee352ee3e95febc88d07930abba2da1b4
picwind <hi.wangwenjun@gmail.com>
2019-05-31 22:48:06 +0800

    Enter

--- 8051/maopao.asm
+++ 8051/maopao.asm
@@ -10,28 +10,28 @@ DUQU:
     MOV     @R1,A;
     INC     R0;
     INC     R1;
-    DJNZ    R2,DUQU;
+    DJNZ    R2,DUQU;读取数据
 
 
 ORG 3000H
 SORT:
-    MOV     R0,#30H
-    MOV     R7,#08H
-    CLR     TR0
+    MOV     R0,#30H;   //数据存储区首单元地址
+    MOV     R7,#08H;   //各次冒泡比较次数
+    CLR     TR0;       //用户标志位清零
 LOOP:
-     MOVB   A,@R0
-     MOV    2BH,A
-     INC    R0
-     MOV    2AH,@R0
-     CLR    C
-     SUBB   A,@R0
-     JC     NEXT
-     MOV    @R0,2BH
-     DEC    R0
-     MOV   @R0,2AH
-     INC    R0
-     SETB   TR0
+     MOVB   A,@R0;      //取前数
+     MOV    2BH,A;      //存前数
+     INC    R0;
+     MOV    2AH,@R0;    //取后数
+     CLR    C;          //进位标志位清零
+     SUBB   A,@R0;      //前数减后数
+     JC     NEXT;       //前数小于后数，不互换，大于后数，互换
+     MOV    @R0,2BH;
+     DEC    R0;
+     MOV   @R0,2AH;     //两个数交换位置
+     INC    R0;         //准备下一次比较
+     SETB   TR0;        //置互换标志
 NEXT:
-    DJNZ    R7,LOOP
-    JB      TR0,SORT
-HERE: SJMP  HERE
+    DJNZ    R7,LOOP;    //返回进行下一次比较
+    JB      TR0,SORT;   //返回，进行下一轮冒泡
+HERE: SJMP  HERE;       //排序结束
